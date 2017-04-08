6a61ebd2ff7c0db749a8276825b295a2e633c12a
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-04-08 17:16:28 +0800

    added company_name, location, published_time to job

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -58,7 +58,7 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden, :company_name)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden, :company_name, :location, :published_time)
   end
 
 end
--- db/migrate/20170325092240_add_more_details_to_job.rb
+++ db/migrate/20170325092240_add_more_details_to_job.rb
@@ -3,8 +3,5 @@ class AddMoreDetailsToJob < ActiveRecord::Migration[5.0]
     add_column :jobs, :wage_upper_bound, :integer
     add_column :jobs, :wage_lower_bound, :integer
     add_column :jobs, :contact_email, :string
-    add_column :jobs, :company_name, :string
-    add_column :jobs, :location, :string
-    add_column :jobs, :published_time, :string
   end
 end
